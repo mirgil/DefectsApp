209b7e6475246db31dcd4a8eb7f881f99ac488be
Gil Mirmovitch <gil.mir@gmail.com>
2024-01-04 14:40:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace DefectsApp
 {
     public class Program
     {
+        
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
